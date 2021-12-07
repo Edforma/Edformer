3e3cbfa2a10ed83cb0dc95c618770d8399a4e7f0
Griffin Bauer <griffinrbauer@icloud.com>
2021-12-06 18:58:07 -0600

    own up to my mistakes

--- logger.js
+++ logger.js
@@ -4,6 +4,7 @@ const winston = require('winston');
 winston.add(new winston.transports.File({ filename: `./logs/${new Date().toISOString()}.log` })) // Log to a file
 
 // If we are not in production, log to console with a readable format.
+// TODO: Make this work
 if (process.env.NODE_ENV !== 'production') {
   winston.add(new winston.transports.Console({
     format: winston.format.cli(),

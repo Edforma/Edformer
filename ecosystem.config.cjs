76853f6e5127e8ed36304298bd3c2c952e95e716
Griffin Bauer <griffin@edforma.app>
2023-06-04 11:10:15 -0500

    ecosystem update, rehiding

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -17,7 +17,8 @@ module.exports = {
       repo : 'https://github.com/Edforma/Edformer.git',
       path : '/home/griffin/pm2Data/Edforma',
       'pre-setup': "ls -la && pwd && uname -a && yarn --version",
-      'post-deploy' : 'yarn && pm2 reload ecosystem.config.cjs --env production',
+      "pre-deploy-local": "echo Uploading ecosystem... && scp ecosystem.config.cjs griffin@unsec.griffinbauer.com:'/home/griffin/' && Configuration uploaded.",
+      'post-deploy' : 'yarn && pm2 reload ~/ecosystem.config.cjs --env production',
       "env": {
         "SENTRY_DSN": "https://0577db8058fb43728025ccc85fc11439@o1069103.ingest.sentry.io/6065463",
         "SENTRY_TRACESAMPLERATE": 0.5,

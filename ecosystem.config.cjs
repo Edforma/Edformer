fda7b75f973ddcb62991a28e9d5ce88fae74876b
Griffin Bauer <griffin@edforma.app>
2023-06-04 11:11:39 -0500

    fix typo

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -17,7 +17,7 @@ module.exports = {
       repo : 'https://github.com/Edforma/Edformer.git',
       path : '/home/griffin/pm2Data/Edforma',
       'pre-setup': "ls -la && pwd && uname -a && yarn --version",
-      "pre-deploy-local": "echo Uploading ecosystem... && scp ecosystem.config.cjs griffin@unsec.griffinbauer.com:'/home/griffin/' && Configuration uploaded.",
+      "pre-deploy-local": "echo Uploading ecosystem... && scp ecosystem.config.cjs griffin@unsec.griffinbauer.com:'/home/griffin/' && echo Configuration uploaded.",
       'post-deploy' : 'yarn && pm2 reload ~/ecosystem.config.cjs --env production',
       "env": {
         "SENTRY_DSN": "https://0577db8058fb43728025ccc85fc11439@o1069103.ingest.sentry.io/6065463",

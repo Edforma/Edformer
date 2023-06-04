9d9dc9c0ad74ab71ccc71d5697c2e644792283cf
Griffin Bauer <griffin@edforma.app>
2023-06-03 21:33:37 -0500

    Nevermind, push ecosystem

---
+++ ecosystem.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  apps : [{
+    script: 'index.js',
+    watch: '.'
+  }],
+
+  deploy : {
+    production : {
+      user : 'griffin',
+      host : 'unsec.griffinbauer.com',
+      ref  : 'origin/v2',
+      repo : 'https://github.com/Edforma/Edformer.git',
+      path : '/home/griffin/pm2Data/Edforma',
+      'pre-setup': "ls -la && pwd && uname -a && yarn --version",
+      'post-deploy' : 'yarn && pm2 reload ecosystem.config.cjs --env production'
+    }
+  }
+};

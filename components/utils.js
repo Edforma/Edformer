8df0746d44665297343e38b24178119a70ef35df
Griffin Bauer <griffin@edforma.app>
2023-07-08 17:17:13 -0500

    add some new login stuff

--- components/utils.js
+++ components/utils.js
@@ -9,6 +9,8 @@ import { randomUUID } from "crypto";
 import winston from 'winston'
 import { wrapper } from 'axios-cookiejar-support';
 const db = new PouchDB('data')
+import url from "url";
+
 
 /**
  * Obtains the raw cookie data that belongs to an access token.
@@ -30,7 +32,7 @@ const _authCookie = async (accessToken) => {
 function formatDate(dateString) {
     const [month, day, year] = dateString.split('/');
     return new Date(`${month}/${day}/${year}`).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' });
-  }
+}
 
 /**
  * Authenticate a student with the Conroe ISD servers. 
@@ -38,33 +40,243 @@ function formatDate(dateString) {
  * @param {string} psw Password of an Student Access Center account.
  */
 const login = async (usr, psw, res) => {
-    const jar = new CookieJar()
-    const axiosTc = wrapper(axios.create({ jar }))
+    let csrfToken = ''
+    let login_url = ''
+    let bearer = ''
+    let SAMLRes = ''
 
-    // Assemble parameters; I'm unsure if we exactly *need* the __EVENT... or __ASYNCPOST parameters or not; I'm keeping them here as it's what
-    // CL sends, and we want to replicate the login flows for CL as close as possible to ensure everything works right.
-    let dataToSend = new URLSearchParams({
-        stuuser: usr,
-        password: psw
+    let masqueradeConfig = {
+        // Masquerade as a mobile device (iPhone SE 2nd Generation).
+        // The os, browser and resolution are only sent on the first request to Classlink to define the login device for the session.
+        // This will be shown in the Login History section of My Analytics.
+        os: 'iOS',
+        browser: 'Mobile Safari',
+        resolution: '375x667',
+        // The user agent will be sent on *all* requests to Classlink and Conroe ISD, to match with the login device details.
+        // Using the user agent from a physical device is recommended.
+        userAgent: "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
+    }
+
+    const jar = new CookieJar()
+    let axiosCJ = wrapper(axios.create({ jar }))
+    axiosCJ.defaults.headers.common['User-Agent'] = masqueradeConfig.userAgent;
+
+    // Adds some cookies to the jar, and sets the csrfToken variable.
+    await axiosCJ.request({
+        method: 'GET',
+        url: 'https://launchpad.classlink.com/conroeisd',
+    }).then((response) => {
+        let $ = cheerioLoad(response.data)
+        // The token is in a script tag that defines the object IdConfig, which csrfToken is a property of.
+        let script = $('script:contains("IdConfig")').html()
+        csrfToken = JSON.parse(script.split('var IdConfig = ')[1].replace(';', '')).csrfToken // Ugh.
+
+        winston.info(`clsession is ${jar.store.idx['classlink.com']['/']['clsession'].value}`)
+        winston.info(`_csrf is ${jar.store.idx['launchpad.classlink.com']['/']['_csrf'].value}`)
+        winston.info(`baseurl is ${jar.store.idx['launchpad.classlink.com']['/']['baseurl'].value}`)
+        winston.info('Trying to get csrfToken from webpage...')
+        winston.info(`csrfToken is ${csrfToken}`)
     })
 
-    await axiosTc.post('https://pac.conroeisd.net/sso.asp?u=1', dataToSend.toString(), {
+    // Grab our login_url from the Classlink API.
+    let fetchLoginUrlResponse = await axiosCJ.request({
+        method: 'POST',
+        url: 'https://launchpad.classlink.com/login',
         headers: {
-            accept: '*/*',
-            'content-type': 'application/x-www-form-urlencoded'
+            Accept: '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'csrf-token': csrfToken,
+            Connection: 'keep-alive'
+        },
+        data: {
+            username: usr,
+            password: psw,
+            os: masqueradeConfig.os,
+            userdn: '', // I'm unsure what this is. User Display Name?
+            code: 'conroeisd',
+            Browser: masqueradeConfig.browser,
+            Resolution: masqueradeConfig.resolution
         }
-    }).then((r) => {
-        // Looks for a response regarding invalid creds
-        // "User not found or incorrect information."
-        if (r.data.indexOf("User not found or incorrect information.") >= 0) {
-            // Set the status code to 401, and send the error message.
-            return res.status(401).send({ status: "failed", error: "User not found or incorrect information." })
+    }).catch((error) => {
+        winston.error(`Got error ${error.response.status} ${error.response.statusText} while fetching login uri!`)
+        if (typeof fetchLoginUrlResponse.data === 'object' && fetchLoginUrlResponse.data.ResultCode != 1) {
+            winston.error(`Classlink rejected authentication attempt:`)
+            winston.error(error.response.data.ResultDescription)
+            res.status(400).send({
+                status: "failed",
+                error: error.response.data.ResultDescription
+            });
+            return;
         }
+    })
 
-        // Just some cookie storage
-        let cookieInfo = jar.toJSON().cookies[0]
+    login_url = fetchLoginUrlResponse.data.login_url
+    winston.info(`Got login_url: ${login_url}`)
+
+    let primeSessionResponse = await axiosCJ.request({
+        method: 'GET',
+        url: 'https://launchpad.classlink.com/oauth2/v2/auth',
+        params: {
+            scope: 'full',
+            redirect_uri: 'https://myapps.classlink.com/oauth/',
+            client_id: 'c1587556210325442f066178b254bbc92cdc0e3e35a9c2',
+            response_type: 'code'
+        },
+        headers: {
+            Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            Referer: 'https://launchpad.classlink.com/conroeisd?loggedout=1',
+            'Alt-Used': 'launchpad.classlink.com',
+            Connection: 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Sec-Fetch-Dest': 'document',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-User': '?1',
+            TE: 'trailers'
+        }
+    }).catch((error) => {
+        winston.error(`Got error ${error.response.status} ${error.response.statusText} while priming session:`)
+        console.dir(error.response.data)
+        res.status(400).send({
+            status: "failed",
+            error: error.response.data
+        });
+    })
+
+    let code = url.parse(primeSessionResponse.request.res.responseUrl, true).query.code
+    winston.info(`Got oauth code: ${code}`)
+
+    let provisionResponse = await axiosCJ.request({
+        method: 'GET',
+        url: 'https://applications.apis.classlink.com/exchangeCode',
+        params: { code, response_type: 'code' },
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0',
+            Accept: 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'application/json',
+            Origin: 'https://myapps.classlink.com',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            Connection: 'keep-alive'
+        }
+    }).catch((error) => {
+        winston.error(`Got error ${error.response.status} ${error.response.statusText} while fetching session bearer:`)
+        console.dir(error.response.data)
+    })
+
+    bearer = provisionResponse.data.token
+    winston.info(`Bearer token is ${bearer}`)
+
+    let sessionStarterResponse = await axiosCJ.request({
+        method: 'POST',
+        url: 'https://applications.apis.classlink.com/v1/startSession',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/113.0',
+            Accept: 'application/json, text/plain, */*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Content-Type': 'text/html',
+            Origin: 'https://myapps.classlink.com',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            Connection: 'keep-alive',
+            Authorization: `Bearer ${bearer}`
+        },
+        data: '{}'
+    }).catch((error) => {
+        winston.error(`Got error ${error.response.status} ${error.response.statusText} while fetching starting session:`)
+        console.dir(error.response.data)
+    })
+
+    if (sessionStarterResponse.data.includes('Session Started')) {
+    }
+
+    let fetchSAMLResResponse = await axiosCJ.request({
+        method: 'GET',
+        url: 'https://idp.classlink.com/sso/select/NGlYeHJNV3RhSUk9'
+    }).catch((error) => {
+        winston.error(`Got error ${error.response.status} ${error.response.statusText} while fetching SAMLResponse:`)
+        console.dir(error.response.data)
+    })
+
+    let $samlres = cheerioLoad(fetchSAMLResResponse.data)
+
+    SAMLRes = $samlres('input[name="SAMLResponse"]').val()
+    winston.info(`Got SAMLResponse with length of ${SAMLRes.length}`)
+
+    let stuConsumeResponse = await axiosCJ.request({
+        method: 'POST',
+        url: 'https://pac.conroeisd.net/Saml/StuConsume.aspx',
+        headers: {
+            authority: 'pac.conroeisd.net',
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'accept-language': 'en-US,en;q=0.9',
+            'cache-control': 'max-age=0',
+            'content-type': 'application/x-www-form-urlencoded',
+            origin: 'https://idp.classlink.com',
+            referer: 'https://idp.classlink.com/',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'cross-site',
+            'upgrade-insecure-requests': '1'
+        },
+        data: {
+            SAMLResponse: SAMLRes,
+            method: 'POST'
+        }
+    }).catch((error) => {
+        winston.error(`Got error ${error.response.status} ${error.response.statusText} while calling StuConsume:`)
+        console.dir(error.response.data)
+    })
+
+    winston.info(`${stuConsumeResponse.status} ${stuConsumeResponse.statusText} in StuConsume, trying to make session...`);
+
+    let $ = cheerioLoad(stuConsumeResponse.data);
+
+    let viewState = $('#__VIEWSTATE').val();
+    let viewStateGenerator = $('#__VIEWSTATEGENERATOR').val();
+    let SAMLAssertion = $('input[name="SAMLAssertion"]').val();
+
+    winston.info(`VIEWSTATE: ${viewState}`);
+    winston.info(`VIEWSTATEGENERATOR: ${viewStateGenerator}`);
+    winston.info(`SAMLAssertion: ${SAMLAssertion}`);
+
+    let consumeResponse = await axiosCJ.request({
+        method: 'POST',
+        url: 'https://pac.conroeisd.net/Consume.asp',
+        headers: {
+            'cache-control': 'max-age=0',
+            accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-encoding': 'gzip, deflate, br',
+            'accept-language': 'en-US',
+            'content-type': 'application/x-www-form-urlencoded',
+            referer: 'https://pac.conroeisd.net/Saml/StuConsume.aspx',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'same-origin',
+            'upgrade-insecure-requests': '1',
+        },
+        data: {
+            __VIEWSTATE: viewState,
+            __VIEWSTATEGENERATOR: '4072EE9F',
+            SAMLAssertion: SAMLAssertion
+        }
+    }).catch((error) => {
+        error(`Got error ${error.response.status} ${error.response.statusText} while calling Consume:`)
+        console.dir(error.response.data)
+    })
+
+    if (consumeResponse.data.includes('This page uses frames, but your browser doesn\'t support them.')) {
+        let cookieInfo = jar.toJSON().cookies[5]
         let accessToken = randomUUID()
-
         db.put({
             "_id": "session-" + accessToken,
             cookieData: { name: cookieInfo.key, token: cookieInfo.value }
@@ -72,7 +284,11 @@ const login = async (usr, psw, res) => {
             return winston.error(`${usr} - Failed to put session doc. ${e}`)
         })
         res.send({ status: "success", accessToken: accessToken });
-    })
+    } else {
+        winston.error('Expected redirect to loader page but got this:')
+        winston.info(consumeResponse.data)
+        winston.error('The SAMLResponse may be invalid.')
+    }
 }
 
 /**
@@ -100,9 +316,9 @@ const getStudentData = async (token, res) => {
     const $ = cheerioLoad(studentInfoPage.data);
 
     const getTableValue = (key) => {
-        return $(`td:contains(${key})`).filter(function() {
+        return $(`td:contains(${key})`).filter(function () {
             return $(this).text().trim() === key
-        }).next().text() 
+        }).next().text()
     }
 
     // winston.info(getTableValue('Student'))
@@ -183,56 +399,56 @@ const getGrades = async (token, res) => {
         });
         return;
     }
-    
+
     let $ = cheerioLoad(page.data)
 
     let courses = [];
-    $('table:contains(Class Averages) tr').each(function(index) {
-    if (index > 1) {
-        // Extract the data from the row
-        let period = $(this).find('td:eq(0)').text().trim();
-        let subject = $(this).find('td:eq(1)').text().trim();
-        let course = $(this).find('td:eq(2)').text().trim();
-        let teacher = $(this).find('td:eq(3)').text().trim();
-        let email = $(this).find('td:eq(4) a').attr('href').replace("mailto:", "")
-        let average = parseFloat($(this).find('td:eq(5)').text().trim());
+    $('table:contains(Class Averages) tr').each(function (index) {
+        if (index > 1) {
+            // Extract the data from the row
+            let period = $(this).find('td:eq(0)').text().trim();
+            let subject = $(this).find('td:eq(1)').text().trim();
+            let course = $(this).find('td:eq(2)').text().trim();
+            let teacher = $(this).find('td:eq(3)').text().trim();
+            let email = $(this).find('td:eq(4) a').attr('href').replace("mailto:", "")
+            let average = parseFloat($(this).find('td:eq(5)').text().trim());
 
-        // Create a course object and add it to the array
-        let courseObj = {
-        period: period,
-        subject: subject,
-        course: course,
-        teacher: teacher,
-        teacherEmail: email,
-        average: average
-        };
-        courses.push(courseObj);
-    }
+            // Create a course object and add it to the array
+            let courseObj = {
+                period: period,
+                subject: subject,
+                course: course,
+                teacher: teacher,
+                teacherEmail: email,
+                average: average
+            };
+            courses.push(courseObj);
+        }
     });
 
     let assignments = [];
-    $('table:contains(Description) tr').each(function(index) {
-    if (index >= 3) {
-        // Extract the data from the row
-        let dueDate = $(this).find('td:eq(0)').text().trim();
-        let assignedDate = $(this).find('td:eq(1)').text().trim();
-        let assignmentName = $(this).find('td:eq(2)').text().trim();
-        let courseId = $(this).find('td:eq(3)').text().trim();
-        let category = $(this).find('td:eq(5)').text().trim();
-        let score = isNaN(parseFloat($(this).find('td:eq(6)').text().trim())) ? $(this).find('td:eq(6)').text().trim() : parseFloat($(this).find('td:eq(6)').text().trim()); // This feels really weird and I don't like it. It gets the job done though.
-        let percent = parseFloat($(this).find('td:eq(11)').text().trim());
-        // Create a course object and add it to the array
-        let assignmentObj = {
-            dueDate: dueDate,
-            assignedDate: assignedDate,
-            assignmentName: assignmentName,
-            courseId: courseId,
-            category: category,
-            score: score,
-            percentage: percent
-        };
-        assignments.push(assignmentObj);
-    }
+    $('table:contains(Description) tr').each(function (index) {
+        if (index >= 3) {
+            // Extract the data from the row
+            let dueDate = $(this).find('td:eq(0)').text().trim();
+            let assignedDate = $(this).find('td:eq(1)').text().trim();
+            let assignmentName = $(this).find('td:eq(2)').text().trim();
+            let courseId = $(this).find('td:eq(3)').text().trim();
+            let category = $(this).find('td:eq(5)').text().trim();
+            let score = isNaN(parseFloat($(this).find('td:eq(6)').text().trim())) ? $(this).find('td:eq(6)').text().trim() : parseFloat($(this).find('td:eq(6)').text().trim()); // This feels really weird and I don't like it. It gets the job done though.
+            let percent = parseFloat($(this).find('td:eq(11)').text().trim());
+            // Create a course object and add it to the array
+            let assignmentObj = {
+                dueDate: dueDate,
+                assignedDate: assignedDate,
+                assignmentName: assignmentName,
+                courseId: courseId,
+                category: category,
+                score: score,
+                percentage: percent
+            };
+            assignments.push(assignmentObj);
+        }
     });
 
     // console.log(assignments)
@@ -289,7 +505,7 @@ const getSchedule = async (token, res) => {
     let sem2 = []
 
     // Semester 1
-    $('tr[bgcolor="lightgrey"]').each(function() {
+    $('tr[bgcolor="lightgrey"]').each(function () {
         sem1.push({
             period: $(this).find('td:nth-child(1)').text(),
             subject: $(this).find('td:nth-child(2)').text(),
@@ -301,7 +517,7 @@ const getSchedule = async (token, res) => {
     });
 
     // Semester 2
-    $('tr[bgcolor="whitesmoke"]').each(function() {
+    $('tr[bgcolor="whitesmoke"]').each(function () {
         sem2.push({
             period: $(this).find('td:nth-child(1)').text(),
             subject: $(this).find('td:nth-child(2)').text(),
